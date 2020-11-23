cf39ccc7e004d077e04afce0ae27a316fb309602
walberst <antoniowalber3000@hotmail.com.br>
2020-11-23 19:41:18 -0300

    :3

--- _funcPHP/_Ajax/sendMail.php
+++ _funcPHP/_Ajax/sendMail.php
@@ -16,15 +16,15 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = '';                     // SMTP username
-    $mail->Password   = '';                               // SMTP password
+    $mail->Username   = 'walberst4@gmail.com';                     // SMTP username
+    $mail->Password   = '71bimtzz';                               // SMTP password
    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     $mail->CharSet = 'UTF-8';                                 
 
     //Recipients
-    $mail->setFrom('', 'Contato site');
-    $mail->addAddress('');
+    $mail->setFrom('walberst4@gmail.com', 'Contato site');
+    $mail->addAddress('walberst4@gmail.com');
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
